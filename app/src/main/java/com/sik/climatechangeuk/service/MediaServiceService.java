ff934966bfba451c4bd62fa390c09ad1f5aaf6c2
Si Kusners <simon.kusners@gmail.com>
2021-10-27 21:05:49 +0100

    Add met office data gathering classes

--- app/src/main/java/com/sik/climatechangeuk/service/MediaServiceService.java
+++ app/src/main/java/com/sik/climatechangeuk/service/MediaServiceService.java
@@ -1,11 +1,14 @@
 package com.sik.climatechangeuk.service;
 
+import com.sik.climatechangeuk.model.MonthlyWeatherData;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
 @AllArgsConstructor
@@ -25,4 +28,39 @@ public class MediaServiceService {
 //            waypoint.setImageUrl(imageUrl.orElse(null));
 //        });
 //    }
+
+    @Component
+    public static class MetoDataScheduler {
+
+        @Scheduled(cron = "0 0/1 * * * *")
+        public void doStuff() throws IOException {
+            MetoDataHandler manager = new MetoDataHandler();
+            MetoDataUtilities utilities = new MetoDataUtilities();
+            MetoExcelWriter excelWriter = new MetoExcelWriter();
+
+            Set<MonthlyWeatherData> weatherData = manager.getMonthlyData();
+
+            //Set<MonthlyWeatherData> locationSpecificData = utilities.filterByStation(weatherData, "Paisley");
+
+            Map<String,Set<MonthlyWeatherData>> dataByLocation = weatherData.stream()
+                    .sorted()
+                    .collect(Collectors.groupingBy(MonthlyWeatherData::getStationName,Collectors.toSet()));
+
+            LinkedHashMap<String, Set<MonthlyWeatherData>> sortedMap = new LinkedHashMap<>();
+
+            dataByLocation.entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByKey(Comparator.naturalOrder()))
+                    .forEachOrdered(x -> sortedMap.put(x.getKey(), x.getValue()));
+
+            excelWriter.writeHistoricWorkbook(sortedMap);
+
+            excelWriter.writeAveragesWorkbook(sortedMap);
+
+            excelWriter.writeExtremesWorkbook(utilities.buildExtremesMap(weatherData));
+
+        }
+
+
+    }
 }
---
+++ app/src/main/java/com/sik/climatechangeuk/service/MetoDataScheduler.java
@@ -0,0 +1,47 @@
+package com.sik.climatechangeuk.service;
+
+import com.sik.climatechangeuk.model.MonthlyWeatherData;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+public class MetoDataScheduler {
+
+    @Scheduled(cron = "0 0/1 * * * *")
+    public void doStuff() throws IOException {
+        MetoDataHandler manager = new MetoDataHandler();
+        MetoDataUtilities utilities = new MetoDataUtilities();
+        MetoExcelWriter excelWriter = new MetoExcelWriter();
+
+        Set<MonthlyWeatherData> weatherData = manager.getMonthlyData();
+
+        //Set<MonthlyWeatherData> locationSpecificData = utilities.filterByStation(weatherData, "Paisley");
+
+        Map<String,Set<MonthlyWeatherData>> dataByLocation = weatherData.stream()
+                .sorted()
+                .collect(Collectors.groupingBy(MonthlyWeatherData::getStationName,Collectors.toSet()));
+
+        LinkedHashMap<String, Set<MonthlyWeatherData>> sortedMap = new LinkedHashMap<>();
+
+        dataByLocation.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey(Comparator.naturalOrder()))
+                .forEachOrdered(x -> sortedMap.put(x.getKey(), x.getValue()));
+
+        excelWriter.writeHistoricWorkbook(sortedMap);
+
+        excelWriter.writeAveragesWorkbook(sortedMap);
+
+        excelWriter.writeExtremesWorkbook(utilities.buildExtremesMap(weatherData));
+
+    }
+
+
+}
